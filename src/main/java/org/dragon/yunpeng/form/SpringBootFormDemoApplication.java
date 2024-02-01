57ee9c41fb5d18cfef9d3b339222e298693b4a8c
Yunpeng Li <yli4@umbc.edu>
2024-02-01 12:13:35 -0500

    Launch browser and open home page after Spring Boot application starts.

--- src/main/java/org/dragon/yunpeng/form/SpringBootFormDemoApplication.java
+++ src/main/java/org/dragon/yunpeng/form/SpringBootFormDemoApplication.java
@@ -1,5 +1,10 @@
 package org.dragon.yunpeng.form;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +13,33 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication 
+@SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan(basePackages={"org.dragon.yunpeng.form"})
-@EnableJpaRepositories(basePackages="org.dragon.yunpeng.form.repositories")
+@ComponentScan(basePackages = { "org.dragon.yunpeng.form" })
+@EnableJpaRepositories(basePackages = "org.dragon.yunpeng.form.repositories")
 @EnableTransactionManagement
-@EntityScan(basePackages="org.dragon.yunpeng.form.entities")
+@EntityScan(basePackages = "org.dragon.yunpeng.form.entities")
 public class SpringBootFormDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFormDemoApplication.class, args);
+
+		openHomePage();
 	}
 
+	// Launch browser and open home page after Spring Boot application starts.
+	private static void openHomePage() {
+		try {
+			URI homepage = new URI("http://localhost:8081/SpringBoot_Form/forms");
+
+			System.setProperty("java.awt.headless", "false");
+
+			Desktop.getDesktop().browse(homepage);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
